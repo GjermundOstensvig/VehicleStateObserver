ed53f194510a39ce2028ce0136fa2959752e006d
GjermundOstensvig <gjermund1995@gmail.com>
2023-05-10 11:36:11 -0700

    renaming of dyna4_projects folder

--- duna4_projects/AutomatedDatasetCreation/Database/VehicleDatabase/PassengerCar/AnimationModules/AnimationContentVehicle/VW_Golf_Sportsvan_2015_noForces.m
+++
@@ -1,6 +0,0 @@
-% Vector DYNA4 Version 5.2.0
-USER_COMMENT.Comment = 'User-defined comment';
-USER_COMMENT.v = '';
-AnimationData.animationFile.Comment = 'Animation template file';
-AnimationData.animationFile.v = 'VW_Golf_Sportsvan_2015_noForces.dapx';
-AnimationData.animationFile.vPath = 'VehicleDatabase\PassengerCar\AnimationModules\AnimationContentVehicle';
--- duna4_projects/AutomatedDatasetCreation/Database/VehicleDatabase/PassengerCar/AnimationModules/AnimationContentVehicle/Volvo_XC90_SUV_2015.m
+++
@@ -1,6 +0,0 @@
-% Vector DYNA4 Version 5.2.0
-USER_COMMENT.Comment = 'User-defined comment';
-USER_COMMENT.v = '';
-AnimationData.animationFile.Comment = 'Animation template file';
-AnimationData.animationFile.v = 'Volvo_XC90_SUV_2015.dapx';
-AnimationData.animationFile.vPath = 'VehicleDatabase\PassengerCar\AnimationModules\AnimationContentVehicle';
